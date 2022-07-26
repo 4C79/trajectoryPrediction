28a1e873958807de6a80813b0f6aaa49ae988faa
4C79 <122875421@qq.com>
2022-07-26 20:53:30 +0800

    默认变更列表

--- trajectoryPrediction/display.py
+++ trajectoryPrediction/display.py
@@ -99,7 +99,7 @@ class Ui_MainWindow(object):
 
     def get_file(path):  # 获取所有文件
         all_file = []
-        for f in os.listdir(path):  #  listdir返回文件中所有目录
+        for f in os.listdir(path):  # listdir返回文件中所有目录
             f_name = os.path.join(path, f)
             all_file.append(f_name)
         return all_file
